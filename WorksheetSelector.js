086d512a020717b95f9bc4114d4a08871cfac13d
summerce-platform <68770005+summerce-platform@users.noreply.github.com>
2020-07-27 14:06:06 +0900

    Add files via upload

--- WorksheetSelector.js
+++ WorksheetSelector.js
@@ -40,7 +40,7 @@
               var storedName = sessionStorage.getItem('name');
               var passData = storedName;
               let iframe = document.getElementById('qwer-iframe').contentWindow;
-    iframe.postMessage(passData, 'https://summerce-platform.github.io/tableautest/tableauiframe.html');
+    iframe.postMessage(passData, 'http://mirs.co.kr:8085/tableauiframe.html');
 
             //   $.ajax({ 
              
